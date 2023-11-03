3e54b4895876fde992ce5446111ebf84cf7e519a
Prason Pandey <120626562+itsprason@users.noreply.github.com>
2023-11-03 13:37:00 +0545

    Update spec.cy.js

--- cypress/e2e/spec.cy.js
+++ cypress/e2e/spec.cy.js
@@ -13,7 +13,7 @@ describe('template spec', () => {
     cy.get('button').contains('Run').click()
     //wait for 2 seconds for the execution to complete
 
-    cy.wait(2000)
+    cy.wait(1)
 
     //condition to check if the output shows up
     if (cy.get('.xterm-rows').contains('Hello, World!')) {
